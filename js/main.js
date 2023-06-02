df8741b07cb510df7e3721b7a02da1ac91f452fc
Martinraju11 <martinraju53@gmail.com>
2023-06-02 17:45:21 +0530

    update

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$('.resp').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        620:{
+            items:2,
+            nav:true
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+})
